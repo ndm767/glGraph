40a4536c687dab653f9d956a2a4e7517e3ccf612
ndm767 <5031736+ndm767@users.noreply.github.com>
2021-06-13 15:12:34 -0400

    pruned todo list to remove things that, while nice, don't really need to be done

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,9 @@ int main(int argc, char *argv[]) {
 
   // TODO: axis labeling
   // TODO: axis grid
-  // TODO: add ability to specify screen dimensions
   // TODO: add proper makefile
-  // TODO: make it so not all equations update when you update a single equation
   // TODO: add ability to remove lines
-  // TODO: don't render lines that are completely off screen
-  // TODO: change movement to be more manageable
   // TODO: make it so unit isn't completely reevaluated every time
-  // TODO: improve performance at high scales
 
   std::vector<std::string> currEqs = {""};
   std::vector<Equation *> eqs = {new Equation("")};
